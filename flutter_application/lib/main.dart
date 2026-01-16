7f0012a396347c11f2d29c5fece5cd8f6387bbf3
Emil <er@ntcon.dk>
2026-01-16 14:33:53 +0100

    duringswim_viewmodel created

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -7,10 +7,11 @@ import 'dart:async';
 part 'view/home_view.dart';
 part 'view/connect_view.dart';
 part 'view/history_view.dart';
-part 'view/during_swim_view.dart';
+part 'view/duringswim_view.dart';
 
 part 'view_model/connect_viewmodel.dart';
 part 'view_model/home_viewmodel.dart';
+part 'view_model/duringswim_viewmodel.dart';
 
 part 'model/movesense_device_manager.dart';
 part 'model/during_swim_model.dart';
