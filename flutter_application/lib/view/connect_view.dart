05052f1bd1c7cde16b98795e4f96e6424df15216
Emil <er@ntcon.dk>
2026-01-18 09:30:41 +0100

    Implemented "press to connect", "connecting" "connected"

--- flutter_application/lib/view/connect_view.dart
+++ flutter_application/lib/view/connect_view.dart
@@ -45,9 +45,15 @@ class _ConnectViewCenterState extends State<ConnectViewCenter> {
     super.dispose();
   }
 
+  String _statusText(ConnectViewModel vm) {
+    if (vm.isConnected) return 'Connected';
+    if (vm.isConnecting) return 'Connecting';
+    return 'Press to Connect';
+  }
+
   @override
   Widget build(BuildContext context) {
-    final vm = widget.viewModel;
+    final viewModel = widget.viewModel;
 
     return Center(
       child: Column(
@@ -55,151 +61,13 @@ class _ConnectViewCenterState extends State<ConnectViewCenter> {
         children: [
           FloatingActionButton(
             heroTag: "connectbutton",
-            onPressed: vm.connect,
+            onPressed: viewModel.connect,
             child: const Icon(Icons.bluetooth),
           ),
           const SizedBox(height: 20),
-          Text(
-            'Connected: ${vm.isConnected}',
-            style: const TextStyle(fontSize: 18),
-          ),
+          Text(_statusText(viewModel), style: const TextStyle(fontSize: 18)),
         ],
       ),
     );
   }
 }
-
-// // A simple card widget to represent swim data
-// class _DeviceCard extends StatelessWidget {
-//   final String text;
-//   const _DeviceCard(this.text);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       width: 300,
-//       height: 50,
-//       child: Card(
-//         color: Color.fromARGB(255, 204, 204, 204),
-//         child: Center(child: Text(text)),
-//       ),
-//     );
-//   }
-// }
-
-// void main() => runApp(const MyApp());
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: ConnectViewScreen(),
-//     );
-//   }
-// }
-
-// class ConnectViewScreen extends StatelessWidget {
-//   const ConnectViewScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Stack(
-//           children: [
-//             // The overview of devices
-//             SingleChildScrollView(
-//               padding: const EdgeInsets.fromLTRB(15, 10, 15, 5),
-//               child: Center(
-//                 child: Column(
-//                   children: [
-//                     // Header area
-//                     SizedBox(
-//                       height: 150,
-//                       width: double.infinity,
-//                       child: Stack(
-//                         children: [
-//                           // Logo
-//                           Positioned(
-//                             top: 0,
-//                             right: 0,
-//                             child: Image.asset(
-//                               'assets/logo.png',
-//                               width: 70,
-//                               height: 70,
-//                               fit: BoxFit.contain,
-//                             ),
-//                           ),
-
-//                           // Title + Bluetooth icon (icon beside the title)
-//                           Positioned(
-//                             top: 100,
-//                             left: 0,
-//                             child: Row(
-//                               children: [
-//                                 Image.asset(
-//                                   'assets/Bluetooth_icon.png',
-//                                   width: 30,
-//                                   height: 30,
-//                                   fit: BoxFit.contain,
-//                                 ),
-//                                 const SizedBox(width: 12),
-//                                 const Text(
-//                                   'Connect to Device',
-//                                   style: TextStyle(
-//                                     fontSize: 30,
-//                                     fontWeight: FontWeight.w600,
-//                                   ),
-//                                 ),
-//                               ],
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-
-//                     const SizedBox(height: 130),
-
-//                     const _DeviceCard('Device 1'),
-//                     const _DeviceCard('Device 2'),
-//                     const _DeviceCard('Device 3'),
-//                     const _DeviceCard('Device 4'),
-//                     const _DeviceCard('Device 5'),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-
-//       // Bottom bar (placeholder for buttons later)
-//       bottomNavigationBar: Container(
-//         height: 80,
-//         padding: const EdgeInsets.symmetric(horizontal: 20),
-//         decoration: const BoxDecoration(
-//           color: Color(0xFFF2F2F2),
-//           border: Border(top: BorderSide(color: Color(0xFFDDDDDD))),
-//         ),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.end,
-//           children: [
-//             InkWell(
-//               onTap: () => Navigator.pop(context),
-//               child: const Row(
-//                 children: [
-//                   Icon(Icons.arrow_back, size: 32),
-//                   SizedBox(width: 8),
-//                   Text('Back', style: TextStyle(fontSize: 18)),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
--- flutter_application/lib/view_model/connect_viewmodel.dart
+++ flutter_application/lib/view_model/connect_viewmodel.dart
@@ -2,6 +2,7 @@ part of '../main.dart';
 
 class ConnectViewModel {
   VoidCallback? _onChange;
+  bool _isConnecting = false;
 
   void bind(VoidCallback onChange) {
     _onChange = onChange;
@@ -14,12 +15,24 @@ class ConnectViewModel {
   }
 
   void _notify() {
+    // device changed → stop "connecting" if now connected
+    if (block.movesenseDeviceManager.device.isConnected) {
+      _isConnecting = false;
+    }
     _onChange?.call();
   }
 
   Future<void> connect() async {
+    if (_isConnecting || isConnected) return;
+
+    _isConnecting = true;
+    _onChange?.call();
+
     await block.movesenseDeviceManager.connect();
+    // final state will come via notify()
   }
 
   bool get isConnected => block.movesenseDeviceManager.device.isConnected;
+
+  bool get isConnecting => _isConnecting;
 }
